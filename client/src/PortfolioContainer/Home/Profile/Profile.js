4ec21ea593a4cf58808a0fa93cde24ea9fa38e14
kaitlyn brangan <87716201+KateHydroJug@users.noreply.github.com>
2022-02-16 16:27:32 -0700

    foundation of heading

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Typical from 'react-typical'
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                    <a href='https://www.facebook.com/katers0259'>
+                        <i className='fa fa-facebook-square'></i>
+                    </a>
+                    <a href='https://www.linkedin.com/in/kaitlyn-branagan-a119891b1/'>
+                        <i className='fa fa-linkedin'></i>
+                    </a>
+                    <a href='https://codepen.io/katers0259'>
+                        <i className='fa fa-codepen'></i>
+                    </a>
+                    <a href='https://www.instagram.com/ohbranagans/'>
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href='https://github.com/katers0259'>
+                        <i className='fa fa-github'></i>
+                    </a>
+                </div>
+            
+            <div className='profile-details-name'>
+                <span className='primary-text'>
+                    {" "}
+                    Hello I'm <span className='highlighted-text'>Kaitlyn</span>
+                </span>
+                </div>
+                <div className='profile-details-role'>
+                    <span className='primary-text'>
+                        {" "}
+                        <h1>
+                        {" "}
+                        <Typical 
+                        loop={Infinity}
+                        steps={[
+                            "React/React Native",
+                            3000,
+                            "Motivated",
+                            3000,
+                            "Full Stack Dev",
+                            3000,
+                        ]}
+                        />
+                        </h1>
+                        <span className='profile-role-tagline'>
+                            Passiong for Full Stack Development.
+                        </span>
+                    </span>
+                </div>
+                <div className='profile-options'>
+                    <button className='btn primary-btn'>
+                        { " " }
+                        Work Together {" "}
+                    </button>
+                    <a href='KaitlynBranagan.pdf' download='Kaitlyn KaitlynBranagan.pdf'>
+                        <button className='btn highlighted-btn'>My Resume</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
