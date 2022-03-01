34151e1bd8ff3c8ac9a2731fb8ba328f550d09c2
kaitlyn brangan <87716201+KateHydroJug@users.noreply.github.com>
2022-03-01 12:00:41 -0700

    troubleshooting why it doesnt scroll on slick

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -2,45 +2,58 @@ import React , {useState}from 'react'
 import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
 import ScrollService from '../../utilities/ScrollService'
 import Animations from '../../utilities/Animations'
-
+import './Resume.css'
 
 export default function Resume(props) {
 
     const [selectedBulletIndex, setselectedBulletIndex] = useState(0)
     const [carousalOffSetStyle, setCarousalOffSetStyle] = useState({})
 
+    let fadeInScreenHandler = (screen) =>{
+        if(screen.fadeInScreen !== props.id) return;
+        Animations.animations.fadeInScreen(props.id);
+    };
+
+    const fadeInSubscription = 
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+
+
     const ResumeHeading =(props) => {
-        <div className='resume-heading'>
+        return (
+            <div className='resume-heading'>
             <div className='resume-main-heading'>
-                <div className='heading-bullet'>
+                <div className='heading-bullet'></div>
                     <span>{props.heading ? props.heading : ''}</span>
                     {props.fromDate && props.toDate ? (
-                        <dov className='heading-date'>
-                            {props.fromDate + "_" + props.toDate}
-                        </dov>
+                        <div className='heading-date'>
+                            {props.fromDate + "-" + props.toDate}
+                        </div>
                     ) : ( 
                     <div></div>
                     )}
                 </div>
                 <div className='resume-sub-heading'>
-                    <span>{props.subheading ? props.subheading : ""}</span>
+                    <span>{props.subHeading ? props.subHeading : ""}</span>
                 </div>
                 <div className='resume-heading-description'>
                     <span>{props.description  ? props.description : ""}</span>
                 </div>
             </div>
-        </div>
-    }
+        
+        )
+     
+    };
 
     const resumeBullets = [
         {label: "Education", logoSrc: "education.svg"},
         {label: "Work History", logoSrc:"work-history.svg"},
-        {label: " Programming Skills", logoSrc:"programming-skills.svg"},
+        {label: "Programming Skills", logoSrc:"programming-skills.svg"},
         {label: "Projects", logoSrc:"projects.svg"},
         {label: "Inerests", logoSrc:"interests.svg"},
     ];
 
-const programminigSkillDetails = [
+    const programminigSkillDetails = [
     {skill:"Javascript", ratingPercentage:65},
     {skill: "React Js", ratingPercentage:65},
     {skill: "Shopify Liquid", ratingPercentage:85},
@@ -49,9 +62,9 @@ const programminigSkillDetails = [
     {skill: "Mongo DB", ratingPercentage:55},
     {skill: "HTML", ratingPercentage:100},
     {skill: "CSS", ratingPercentage:95}, 
-];
+    ];
 
-const projectDetails = [
+        const projectDetails = [
     {
         title:"HydroJug Projects", 
         duration:{fromDate: "2021", toDate:"2021"},
@@ -65,9 +78,9 @@ const projectDetails = [
         subheading:"Technologies Used: TailwindCSS, React, HTML, CSS, Javasript, AWS, MongoDB"
     },
 
-];
+        ];
 
-    const resumeDetails = [
+        const resumeDetails = [
         <div className='resume-screen-container' key="education">
             <ResumeHeading
             heading={"University Of Utah, Utah"}
@@ -82,7 +95,9 @@ const projectDetails = [
             toDate={"2010"}
             />
         </div>,
+
         <div className='resume-screen-container' key="work-experience">
+            <div className="experience-container">
           <ResumeHeading
             heading={"HydroJug"}
             subheading={"Web Developer"}
@@ -97,30 +112,35 @@ const projectDetails = [
             <div className='experience-description'>
                 <span className='resume-description-text'>
                 Responsible for all aspects of HydroJug US Site, as well as other assigned tasks for our
-HydroJug UK site as well as some of our other brands. Launching New Products, creating new
-and modern design for all websites brand wide. Helping with UX/UI Design.
-Key Accomplishments:
-● Learning an entirely new language (Shopify Liquid) for this position.
-● Completed 8 Large Projects in less than 9 Months with the Company.
-● Helping Deploy and Run our UK Site.
+                HydroJug UK site as well as some of our other brands. Launching New Products, creating new
+                and modern design for all websites brand wide. Helping with UX/UI Design.
+                </span>
+                <span className='resume-description-text'>
+                Key Accomplishments:
+                ● Learning an entirely new language (Shopify Liquid) for this position.
+                ● Completed 8 Large Projects in less than 9 Months with the Company.
+                ● Helping Deploy and Run our UK Site.
                 </span>
                 <br/>
-                
+                </div>
+                </div>
             </div>,
+
             <div className='resume-screen-container programming-skills-container'
             key="programming-skills">
                 {programminigSkillDetails.map((skill, index)=>(
                     <div className='skill-parent' key={index}>
                         <div className='heading-bullet'></div>
                         <span>{skill.skill}</span>
-                        <div className='skill-percentage'></div>
+                        <div className='skill-percentage'>
                         <div style={{width:skill.ratingPercentage + "%"}}
-                        className='active-percentage'>
-
+                        className='active-percentage-bar'>
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>,
+
             <div className='resume-screen-container' key="projects">
                 {projectDetails.map((projectDetails, index)=>(
                     <ResumeHeading
@@ -147,25 +167,63 @@ Key Accomplishments:
                 heading='Animals'
                 description=" I have a dream one day to run an animal sanctuary."
                 />
-            </div>
-        </div>,
+            </div>,
+        
     ];
 
-    let fadeInScreenHandler = (screen) =>{
-        if(screen.fadeInScreen !== props.id)
-        return
-        Animations.animations.fadeInScreen(props.id)
-    }
+    const handleCarousal = (index) => {
+        let offsetHeight = 360;
+        let newCarousalOffset =  {
+            style: {transorm: "translateY(" + index * offsetHeight * -1 + "px)"},
+        };
+        setCarousalOffSetStyle(newCarousalOffset);
+        setselectedBulletIndex(index);
+    };
 
-    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+    const getBullets = () => {
+        return resumeBullets.map((bullet, index) => (
+            <div 
+            onClick={() => handleCarousal(index)}
+            className={index === selectedBulletIndex ? "bullet selected-bullet" : "bullet"}
+            key={index}>
+                    
+            <img className='bullet-logo' 
+            src={require (`../../assets/Resume/${bullet.logoSrc}`)}
+            alt='no internet connection'
+            />
+            <span className="bullet-label">{bullet.label}</span>
+            </div>
+        ))
+      
+    };
+
+    const getResumeScreens = () =>{
+        return (
+            <div
+            style = {carousalOffSetStyle.style}
+            className='resume-details-carousal'
+            >
+                {resumeDetails.map((ResumeDetail) => ResumeDetail)}
+            </div>
+        );
+    };
 
   return (
-    <div className='resume-container screen-container' id={props.id || ""}>
-        <div className='resume-contents'>
+    <div className='resume-container screen-container fade-in' id={props.id || ""}>
+        <div className='resume-content'>
             <ScreenHeading 
             title={'Resume'}
             subheading={'Bio Details'}
             />
+            <div className='resume-card'>
+                <div className='resume-bullets'>
+                    <div className='bullet-container'>
+                        <div className='bullet-icons'></div>
+                        <div className='bullets'>{getBullets()}</div>
+                    </div>
+                    </div>
+                <div className='resume-bullet-details'>{getResumeScreens()}</div>
+            </div>
             </div> 
     </div>
   )
